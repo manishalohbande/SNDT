3d790b663e20193e1c969b8d36e85bc9d311feef
manishalohbande <71079414+manishalohbande@users.noreply.github.com>
2021-07-08 15:29:17 +0300

    scan data fuctionality added exception hndler and added prn no in dispute action details

---
+++ src/main/java/com/scube/edu/request/AssociateExcelRequest.java
@@ -0,0 +1,204 @@
+package com.scube.edu.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AssociateExcelRequest {
+	
+	private String collegeName;
+	private String stream;
+	private String enrollmentNo;
+	private String passingYear;
+	private String reason;
+	private String branch_nm;
+	private String semester;
+	private String monthOfPassing;
+	private String dataType;
+
+	private String prnNo;
+
+	private String center;
+
+	private String medDesc;
+
+
+	private String studentName;
+
+
+	private String semTotalMax;
+
+	private String semGradeTotal;
+
+	private String semGrace;
+
+	private String semclass;
+
+	private String semGpa;
+
+	private String semGrade;
+
+	private String gradeTotal;
+
+	private String classGrace;
+
+	private String maxGrTotal;
+
+	private String credTotal;
+
+	private String gpa;
+
+	private String fgrade;
+
+	private String resultDesc;
+
+	private String inceDesc;
+
+	private String subOne;
+
+	private String subNmOne;
+
+	private String creditSubOne;
+
+	private String intSubOne;
+
+	private String extSubOne;
+
+	private String totalSubOne;
+
+	private String graceSubOne;
+
+	private String prvFlgSubOne;
+
+	private String maxIntSubOne;
+
+	private String maxExtSubOne;
+
+	private String maxTotalSubOne;
+
+	private String gradeSubOne;
+
+	private String subTwo;
+
+	private String subNmTwo;
+
+	private String creditSubTwo;
+
+	private String intSubTwo;
+
+	private String extSubTwo;
+
+	private String totalSubTwo;
+
+	private String graceSubTwo;
+
+	private String prvFlgSubTwo;
+
+	private String maxIntSubTwo;
+
+	private String maxExtSubTwo;
+
+	private String maxTotalSubTwo;
+	
+	private String gradeSubTwo;
+
+
+	private String subThree;
+
+	private String subNmThree;
+
+	private String creditSubThree;
+
+	private String intSubThree;
+
+	private String extSubThree;
+
+	private String totalSubThree;
+
+	private String graceSubThree;
+
+	private String prvFlgSubThree;
+
+	private String maxIntSubThree;
+
+	private String maxExtSubThree;
+
+	private String maxTotalSubThree;
+
+	private String gradeSubThree;
+
+	private String subFour;
+
+	private String subNmFour;
+
+	private String creditSubFour;
+
+	private String intSubFour;
+
+	private String extSubFour;
+
+	private String totalSubFour;
+
+	private String graceSubFour;
+
+	private String prvFlgSubFour;
+
+	private String maxIntSubFour;
+
+	private String maxExtSubFour;
+
+	private String maxTotalSubFour;
+
+	private String gradeSubFour;
+
+	private String subFive;
+
+	private String subNmFive;
+
+	private String creditSubFive;
+
+	private String intSubFive;
+
+	private String extSubFive;
+
+	private String totalSubFive;
+
+	private String graceSubFive;
+
+	private String prvFlgSubFive;
+
+	private String maxIntSubFive;
+
+	private String maxExtSubFive;
+
+	private String maxTotalSubFive;
+
+	private String gradeSubFive;
+
+	private String subSix;
+
+	private String subNmSix;
+
+	private String creditSubSix;
+
+	private String intSubSix;
+
+	private String extSubSix;
+
+	private String totalSubSix;
+
+	private String graceSubSix;
+
+	private String prvFlgSubSix;
+
+	private String maxIntSubSix;
+
+	private String maxExtSubSix;
+
+	private String maxTotalSubSix;
+	
+	private String gradeSubSix;
+}
